62fc4a9979721b5939d5e61e98c5f090156d75b8
ShadowsAndDust-Maximus <vivmaster7@hotmail.com>
2021-04-06 18:42:22 -0700

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button 
+            @toggle-add-task="toggleUp"
+            :text="showAddTask ? 'Close' : 'Add Task' " 
+            :color="showAddTask ? 'red' : 'green'"
+        />
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+        showAddTask: Boolean,
+    },
+    components: {
+        Button,
+    },
+    methods: {
+        toggleUp() {
+            console.log(12)
+            this.$emit('toggle-add-task')
+        }
+    }
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
